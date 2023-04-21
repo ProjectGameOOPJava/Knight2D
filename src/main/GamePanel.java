5149bef517706406225962558c5938b1e9ef4e1e
DucDuyITITIU21008 <127607170+DucDuyITITIU21008@users.noreply.github.com>
2023-04-21 22:04:47 +0700

    Add Inputs into GamePanel

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,19 +1,46 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
+
 public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+	private int xDelta = 200, yDelta = 200;
+
     
     public GamePanel(){
+        mouseInputs = new MouseInputs(this);
+		addKeyListener(new KeyboardInputs(this));
+		addMouseListener(mouseInputs);
+		addMouseMotionListener(mouseInputs);
+    }
+    
+    public void changeXDelta(int value) {
+		this.xDelta += value;
+		repaint();
+	}
 
+	public void changeYDelta(int value) {
+		this.yDelta += value;
+		repaint();
+	}
+
+	public void setRectPos(int x, int y) {
+		this.xDelta = x;
+		this.yDelta = y;
+		repaint();
     }
 
     public void paintComponent(Graphics g){
 
         super.paintComponent(g);
         g.setColor(Color.RED);
-        g.fillRect(200, 200, 200, 100);
+        g.fillRect(xDelta, yDelta, 200, 100);
        
-    }
 }
