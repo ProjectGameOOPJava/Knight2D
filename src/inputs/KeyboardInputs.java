3a818306afeada2117a97cd26158126921aa2a20
DuckDuy <duckduy045@gmail.com>
2023-04-26 22:21:06 +0700

    Add gravity + now player can jump

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -23,13 +23,10 @@ public class KeyboardInputs implements KeyListener {
 	public void keyReleased(KeyEvent e) {
 		switch (e.getKeyCode()) {
 			case KeyEvent.VK_W:
-				gamePanel.getGame().getPlayer().setUp(false);
+				gamePanel.getGame().getPlayer().setJump(false);
 				break;
 			case KeyEvent.VK_A:
 				gamePanel.getGame().getPlayer().setLeft(false);
-				break;
-			case KeyEvent.VK_S:
-				gamePanel.getGame().getPlayer().setDown(false);
 				break;
 			case KeyEvent.VK_D:
 				gamePanel.getGame().getPlayer().setRight(false);
@@ -45,13 +42,10 @@ public class KeyboardInputs implements KeyListener {
 
 		switch (e.getKeyCode()) {
 			case KeyEvent.VK_W:
-				gamePanel.getGame().getPlayer().setUp(true);
+				gamePanel.getGame().getPlayer().setJump(true);
 				break;
 			case KeyEvent.VK_A:
 				gamePanel.getGame().getPlayer().setLeft(true);
-				break;
-			case KeyEvent.VK_S:
-				gamePanel.getGame().getPlayer().setDown(true);
 				break;
 			case KeyEvent.VK_D:
 				gamePanel.getGame().getPlayer().setRight(true);
