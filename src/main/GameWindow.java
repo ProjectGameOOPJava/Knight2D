302d8ea61277c41aba7bbbde6eba337957986309
Vo <hopekasako@gmail.com>
2023-05-12 23:33:03 +0700

    Add icon for game

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -1,22 +1,28 @@
 package main;
 
+import java.awt.*;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowFocusListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class GameWindow {
     
     private JFrame jframe;
+    ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("icon.png"));
 
     public GameWindow(GamePanel gamePanel){
 
         jframe = new JFrame();
+        
         jframe.setTitle("Knight2D");
+        jframe.setIconImage(icon.getImage());
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.add(gamePanel);
         jframe.setResizable(false);
 		jframe.pack();
+        jframe.setIconImage(icon.getImage());
         jframe.setLocationRelativeTo(null);
         jframe.setVisible(true);
 
